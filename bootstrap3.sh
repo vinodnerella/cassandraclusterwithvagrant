7efab8ef2d5b10d265e437ce64117f673fc78d04
Vinod Nerella <nerellavinod@gmail.com>
2018-05-31 10:30:54 +0530

    changed bootstrap3 file

--- bootstrap3.sh
+++ bootstrap3.sh
@@ -13,12 +13,11 @@ sudo sed -i '/NOPASSWD/s/^#//g' /etc/sudoers
 sudo cp /etc/hosts /etc/hosts.bkp
 sudo cp /vagrant/hosts /etc/hosts
 sudo cp /vagrant/datastax.repo /opt/datastax.repo
-sudo cp /opt/datastax.repo /etc/yum.repos.d/
+sudo cp /opt/datastax3.repo /etc/yum.repos.d/
 sudo cp /vagrant/jdk-8u131-linux-x64.rpm /opt/jdk-8u131-linux-x64.rpm
 sudo cp /vagrant/main.sh /opt/main.sh 
 
 sudo rpm -ivh /opt/jdk-8u131-linux-x64.rpm
-#sudo yum -y install dsc21
 
 sudo yum -y install datastax-ddc
 
